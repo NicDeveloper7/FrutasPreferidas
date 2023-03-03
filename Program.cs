ea4063986935f2f98ec8767939b0969ef1e86d6b
nicholasdev@zohomail.com <e107ti@etec.sp;gov;br>
2023-03-03 20:27:16 -0300

    Minhas frutas preferidas

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information 
+Console.WriteLine("---Minhas frutas preferidas---");
+Console.WriteLine();
+Console.WriteLine("1 - Manga");
+Console.WriteLine("2 - Morango");
+Console.WriteLine("3 - Melancia");
+Console.WriteLine("4 - Abacaxi");
+Console.WriteLine("5 - Maçã");
